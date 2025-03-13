7e668a0675c1724c3f4ce939d65fc2c15d905593
Daniel18UvH <ulisesvh.ti22@utsjr.edu.mx>
2025-03-12 23:13:30 -0600

    add

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideRouter } from '@angular/router'; // Importa provideRouter
+import { routes } from './app.routes'; // Importa las rutas
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), // Configura Zone.js
+    provideRouter(routes) // Configura el enrutamiento con las rutas definidas
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component'; // Importa el componente
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'acerca-de', component: AcercaDeComponent }, // Ruta para "Acerca de..."
+  { path: '', redirectTo: '/', pathMatch: 'full' }, 
+];
